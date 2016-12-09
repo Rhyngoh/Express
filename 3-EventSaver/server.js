c264476b23005ea39a00a99e39edbdb94cfcf47f
Ryan Ngoh <rxn120230@utdallas.edu>
2016-12-09 13:22:29 -0600

    added JAWSDBURL connection

--- 3-EventSaver/server.js
+++ 3-EventSaver/server.js
@@ -17,13 +17,16 @@ app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database: "event_saver_db"
-});
-
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
